0689fd01c3d5d3e3aa8c9a63a893075a1c75b07b
NinjaJc01 <james@cookiehome.co.uk>
2018-11-25 21:54:48 +0000

    (actually) Added pastes clientside code

---
+++ resources/js/paste.js
@@ -0,0 +1,68 @@
+let mDiv = document.querySelector("#mainDiv");
+let urlParams = new URLSearchParams(window.location.search);
+
+class pasteClientside {
+    constructor(id, content, timestamp) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.content = content;
+    }
+    
+    createElem() {
+        let node = document.createElement("div");
+        let timeDiv = document.createElement("div");
+        let contentDiv = document.createElement("div");
+
+        node.id = "paste" + this.id;
+        timeDiv.id = "timeDiv" + this.id;
+        contentDiv.id = "contentDiv" + this.id;
+
+        timeDiv.textContent = this.timestamp;
+        contentDiv.textContent = this.content;
+        contentDiv.classList.add("bg-dark");
+        contentDiv.classList.add("text-light");
+
+        node.appendChild(timeDiv);
+        node.appendChild(contentDiv);
+        return node;
+    }
+}
+
+function onload() {
+    console.log(urlParams.get("id"));
+    if (urlParams.get("id") !== null) {
+        getPaste(urlParams.get("id"));
+    } else console.log("null")
+}
+
+
+async function getPaste(id) {
+    let paste = await fetch("/api/paste/" + id)
+        .then(response => response.json());
+    console.log(paste);
+    let thisPaste = new pasteClientside(
+        paste.id,
+        paste.content,
+        paste.timestamp);
+    mDiv.appendChild(thisPaste.createElem());
+}
+
+async function getPastes() {
+    // let pastes = await fetch("/api/paste/list")
+    //     .then(response => response.json());
+    const response = await fetch('/api/paste/list');
+    const pastes = await response.json();
+    if (pastes !== null) {
+        console.log(pastes);
+        pastes.forEach(element => {
+            let thisPaste = new pasteClientside(
+                element.id,
+                element.content,
+                element.timestamp);
+            mDiv.appendChild(thisPaste.createElem());
+        });
+    } else {
+        console.log("null");
+        mDiv.textContent = "No pastes yet! Check back soon."
+    }
+}
